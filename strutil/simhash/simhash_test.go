dea1714a5ba805e139e33923c9eee24c9e47483c
zhuzongxin <dreamsxin@qq.com>
2024-06-21 09:24:59 +0800

    Add Simhash

--- strutil/simhash/simhash_test.go
+++ strutil/simhash/simhash_test.go
@@ -3,6 +3,7 @@ package simhash
 import (
 	"bufio"
 	"fmt"
+	"regexp"
 	"strings"
 	"testing"
 )
@@ -14,7 +15,15 @@ func simhashString(s string) uint64 {
 	return Hash(scanner)
 }
 
-func TestSimHash(t *testing.T) {
+func simhashString2(s string) uint64 {
+	var r = regexp.MustCompile(`[\w']+(?:\://[\w\./]+){0,1}`)
+	words := r.FindAll([]byte(s), -1)
+
+	fmt.Println("words", words)
+	return Hash(NewSliceScanner(words))
+}
+
+func TestSimSipHash(t *testing.T) {
 
 	h1 := simhashString("Now is the winter of our discontent and also the time for all good people to come to the aid of the party")
 	fmt.Printf("h=%016x\n", h1)
@@ -31,4 +40,27 @@ func TestSimHash(t *testing.T) {
 
 	h4 := simhashString(strings.Repeat("Now is the winter", 241)) // length = 4097
 	fmt.Printf("h=%016x\n", h4)
+
+	h5 := simhashString2("this is a test phrase")
+	fmt.Printf("h5=%016x\n", h5)
+
+	h6 := simhashString2("this is a test phrass")
+	fmt.Printf("h6=%016x\n", h6)
+}
+
+func TestSimHash(t *testing.T) {
+	var docs = [][]byte{
+		[]byte("this is a test phrase"),
+		[]byte("this is a test phrass"),
+		[]byte("foo bar"),
+	}
+
+	hashes := make([]uint64, len(docs))
+	for i, d := range docs {
+		hashes[i] = Simhash(NewWordFeatureSet(d))
+		fmt.Printf("Simhash of %s: %x\n", d, hashes[i])
+	}
+
+	fmt.Printf("Comparison of `%s` and `%s`: %d\n", docs[0], docs[1], Compare(hashes[0], hashes[1]))
+	fmt.Printf("Comparison of `%s` and `%s`: %d\n", docs[0], docs[2], Compare(hashes[0], hashes[2]))
 }
