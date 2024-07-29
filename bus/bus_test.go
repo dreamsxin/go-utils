2dc971f1990c2829aa0d0acfbd27c2241b678b06
zhuzongxin <dreamsxin@qq.com>
2024-07-29 12:50:35 +0800

    Add bus

---
+++ bus/bus_test.go
@@ -0,0 +1,91 @@
+package bus
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+type testQuery struct {
+	ID   int64
+	Resp string
+}
+
+func TestEventPublish(t *testing.T) {
+	bus := ProvideBus()
+
+	var invoked bool
+
+	bus.AddEventListener(func(ctx context.Context, query *testQuery) error {
+		invoked = true
+		return nil
+	})
+
+	err := bus.Publish(context.Background(), &testQuery{})
+	require.NoError(t, err, "unable to publish event")
+
+	require.True(t, invoked)
+}
+
+func TestEventPublish_NoRegisteredListener(t *testing.T) {
+	bus := ProvideBus()
+
+	err := bus.Publish(context.Background(), &testQuery{})
+	require.NoError(t, err, "unable to publish event")
+}
+
+func TestEventCtxPublishCtx(t *testing.T) {
+	bus := ProvideBus()
+
+	var invoked bool
+
+	bus.AddEventListener(func(ctx context.Context, query *testQuery) error {
+		invoked = true
+		return nil
+	})
+
+	err := bus.Publish(context.Background(), &testQuery{})
+	require.NoError(t, err, "unable to publish event")
+
+	require.True(t, invoked)
+}
+
+func TestEventPublishCtx_NoRegisteredListener(t *testing.T) {
+	bus := ProvideBus()
+
+	err := bus.Publish(context.Background(), &testQuery{})
+	require.NoError(t, err, "unable to publish event")
+}
+
+func TestEventPublishCtx(t *testing.T) {
+	bus := ProvideBus()
+
+	var invoked bool
+
+	bus.AddEventListener(func(ctx context.Context, query *testQuery) error {
+		invoked = true
+		return nil
+	})
+
+	err := bus.Publish(context.Background(), &testQuery{})
+	require.NoError(t, err, "unable to publish event")
+
+	require.True(t, invoked)
+}
+
+func TestEventCtxPublish(t *testing.T) {
+	bus := ProvideBus()
+
+	var invoked bool
+
+	bus.AddEventListener(func(ctx context.Context, query *testQuery) error {
+		invoked = true
+		return nil
+	})
+
+	err := bus.Publish(context.Background(), &testQuery{})
+	require.NoError(t, err, "unable to publish event")
+
+	require.True(t, invoked)
+}
